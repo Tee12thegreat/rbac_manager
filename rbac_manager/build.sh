9ba305f5538cd6d687a752bfdd95f9c130aeb7f8
Tee12Thegreat <takazmulla@gmail.com>
2026-06-07 11:29:25 +0200

    commit2

--- rbac_manager/build.sh
+++ rbac_manager/build.sh
@@ -1,13 +1,8 @@
+cat > build.sh << 'EOF'
 #!/usr/bin/env bash
-# Render runs this script every time you deploy.
-# exit immediately if any command fails
 set -o errexit
-
-# Install Python dependencies
 pip install -r requirements.txt
-
-# Collect all static files (CSS, JS) into /staticfiles for WhiteNoise to serve
 python manage.py collectstatic --no-input
-
-# Apply any pending database migrations
 python manage.py migrate
+python manage.py setup_demo
+EOF
